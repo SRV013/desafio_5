7d8aabe627ccf333a6558639900781bcdf5ff735
srv013 <srv013@gmail.com>
2022-09-20 19:34:00 -0300

    arreglo 6

--- src/routes.ts
+++ src/routes.ts
@@ -28,33 +28,24 @@ const routes = [
  const BASE_PATH = "/juego-ppt";
  function isGithubPages() {
    return location.host.includes("github.io");}
-   export function initRouter(container: any) {
-      function goTo(path) {
-          const completePath = isGithubPages() ? BASE_PATH + path : path;
-          history.pushState({}, "", completePath);
-          handleRoute(completePath);
-      }
-      function handleRoute(route) {
-          const newRoute = isGithubPages() ? route.replace(BASE_PATH, "") : route;  
-          for (const r of routes) {
-              if (r.path.test(newRoute)) {
-                  const el: any = r.component({ goTo: goTo });
-                  if (container.firstChild) {
-                      container.firstChild.remove();
-                  }
-                  container.appendChild(el);
-              }
+
+export function initRouter(container: Element) {
+    function goTo(path) {
+      const completePath = isGithubPages() ? BASE_PATH + path : path;
+       history.pushState({}, "", path);
+       handleRoute(path);
+    }
+ 
+    function handleRoute(route) {
+       container.innerHTML = ``;
+       routes.find((r) => {
+          if (r.path.test(route)) {
+             const el = r.component({ goTo: goTo });
+             container.appendChild(el);
           }
-  
-      }
-      if (location.host.includes("github.io")) {
-          goTo("/inicio");
-      } else if (location.pathname == "/") {
-          goTo("/inicio");
-      } else {
-          handleRoute(location.pathname)
-      }
-      window.onpopstate = function () {
-          handleRoute(location.pathname);
-      }
+       });
+    }
+    goTo('/inicio')
+    console.log(location.pathname);
+    handleRoute(location.pathname);
 } 
